06b2d1bccbe90c24e1f9a8067e5b741acecd9a8f
nickbuker <nickbuker@gmail.com>
2016-12-06 11:34:15 -0800

    add sum_dig_power

---
+++ sum_dig_power.py
@@ -0,0 +1,10 @@
+def sum_dig_pow(a, b):
+    results = []
+    for num in xrange(a,b+1):
+        num_list = list(str(num))
+        num_sum = 0
+        for i, dig in enumerate(num_list):
+            num_sum += int(dig) ** (i + 1)
+        if num_sum == num:
+            results.append(num)
+    return results
