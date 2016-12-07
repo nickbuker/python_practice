1ea8b4238fff8ce7ddcc0a067aefad9a56c46d43
nickbuker <nickbuker@gmail.com>
2016-12-06 20:04:01 -0800

    update paren check

--- paren_check.py
+++ paren_check.py
@@ -1,22 +1,12 @@
 def paren_checker(s):
-    lp, lb, lc, rp, rb, rc = 0, 0, 0, 0, 0, 0
+    p = {"(":0, "[":0, "{":0, ")":0, "]":0, "}":0}
     s_list = list(s)
     for char in s_list:
-        if char == "(":
-            lp += 1
-        if char == "[":
-            lb += 1
-        if char == "{":
-            lc += 1
-        if char == ")":
-            rp += 1
-        if char == "]":
-            rb += 1
-        if char == "}":
-            rc += 1
-        if lp < rp or lb < rb or lc < rc:
-            return False
-    if lp != rp or lb != rb or lc != rc:
+        if char in p:
+            p[char] += 1
+            if p["("] < p[")"] or p["["] < p["]"] or p["{"] < p["}"]:
+                return False
+    if p["("] != p[")"] or p["["] != p["]"] or p["{"] != p["}"]:
         return False
     else:
         return True
