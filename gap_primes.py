80fc5b19fbb3ba97a919b26c66935b0a5dade31b
nickbuker <nickbuker@gmail.com>
2016-12-09 09:19:13 -0800

    add gap primes

---
+++ gap_primes.py
@@ -0,0 +1,14 @@
+def gap(g, m, n):
+    p1, p2 = 0, 0
+    for num in xrange(m, n+1):
+        prime = True
+        for x in xrange(2, int(num ** 0.5) +1):
+            if num % x == 0:
+                prime = False
+                break
+        if prime == True:
+            p1 = p2
+            p2 = num
+        if p2-p1 == g:
+            return [p1,p2]
+    return None
