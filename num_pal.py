d2b40948365078ba3d0fb3be6a42cf370a5a8dff
nickbuker <nickbuker@gmail.com>
2016-12-18 08:58:08 -0800

    update palindrome

--- num_pal.py
+++ num_pal.py
@@ -1,17 +1,25 @@
-from collections import Counter
+from collections import Counter,defaultdict
 from operator import mul
 
 def numeric_palindrome(*args):
-    n_list = map(int, list(str(find_n(args))))
+    n_list = map(int, list(str(reduce(mul, args))))
     return max(find_pals(n_list))
 
-def find_n(args):
-    return reduce(mul, args)
-
 def find_pals(n_list):
-    lefts, rights = [], []
-    
+    counts = Counter(n_list)
+    tup = zip(counts.values(), counts.keys())
+    c_dict = reduce(lambda x, (k,v): x[k].append(v) or x, tup, defaultdict(list))
     return pals
+
+def construct_pal(c_dict):
+    lefts, rights = [], []
+    m = max(c_dict)
+    if m % 2 == 0:
+
+
+
+
+
 
 
     # for n in xrange(0, len(n_list)):
