3731f95f4080d97a96594fe383d80994069c5f82
nickbuker <nickbuker@gmail.com>
2016-12-23 16:12:38 -0800

    new solution to factorial trailing zeros

--- src/trailing_zero_factorial.py
+++ src/trailing_zero_factorial.py
@@ -1,2 +1,9 @@
-def count_trailing_zeros(n):
-    return n // 5
+from math import log
+
+def count_trailing_zeros(num):
+    zeros = 0
+    for n in xrange(1, num + 1):
+        for m in xrange(1, int(log(n, 5) + 1)):
+            if n % (5 ** m) == 0:
+                zeros += 1
+    return zeros
