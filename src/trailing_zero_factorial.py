88841f56645769d5ceba6381031aefda15664656
nickbuker <nickbuker@gmail.com>
2016-12-23 19:46:59 -0800

    update factorial

--- src/trailing_zero_factorial.py
+++ src/trailing_zero_factorial.py
@@ -2,7 +2,9 @@ from math import log
 
 def count_trailing_zeros(num):
     zeros = 0
-    for n in xrange(1, num + 1):
+    for n in xrange(0, num + 1, 5):
+        if n == 0:
+            continue
         for m in xrange(1, int(log(n, 5) + 1)):
             if n % (5 ** m) == 0:
                 zeros += 1
