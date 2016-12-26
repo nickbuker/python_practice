6e5d2f9567b36fc36ab2ce6be0ff55b708982f16
nickbuker <nickbuker@gmail.com>
2016-12-26 10:57:01 -0800

    add python practice

---
+++ src/to_sqrt_or_not.py
@@ -0,0 +1,6 @@
+def square_or_square_root(arr):
+    squares = find_squares(arr)
+    return [int(x ** 0.5) if x in squares else x ** 2 for x in arr]
+
+def find_squares(arr):
+    return [x ** 2 for x in xrange(1, int(max(arr) ** 0.5) + 1)]
