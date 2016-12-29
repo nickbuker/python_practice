a529089d3c55db8a9b5628269e0ef16175cfd119
nickbuker <nickbuker@gmail.com>
2016-12-28 18:13:24 -0800

    add python practice

---
+++ src/find_intersection.py
@@ -0,0 +1,65 @@
+from __future__ import division
+
+def inter(xa1,xa2,ya1,ya2,xb1,xb2,yb1,yb2):
+    if vert(xa1,xa2) and vert(xb1,xb2):
+        if between(ya1,ya2,yb1):
+            return xb1, yb1
+        if between(ya1,ya2,yb2):
+            return xb2, yb2
+        else:
+            return "No Intercept"
+
+    if vert(xa1,xa2):
+        mb, bb = slope_int(xb1,xb2,yb1,yb2)
+        i_y = mb * xa1 + bb
+        if between(xb1,xb2,xa1) and between(yb1,yb2,i_y) and between(ya1,ya2,i_y):
+            return xa1, i_y
+        else:
+            return "No Intercept"
+
+    if vert(xb1,xb2):
+        ma, ba = slope_int(xa1,xa2,ya1,ya2)
+        i_y = ma * xb1 + ba
+        if between(xa1,xa2,xb1) and between(ya1,ya2,i_y) and between(yb1,yb2,i_y):
+            return xb1, i_y
+        else:
+            return "No Intercept"
+
+    ma, ba = slope_int(xa1,xa2,ya1,ya2)
+    mb, bb = slope_int(xb1,xb2,yb1,yb2)
+
+    if ma == mb and ba != bb:
+        return "No Intercept"
+    if ma == mb and ba == bb:
+        if between(xa1, xa2, xb1):
+            return xb1, yb1
+        if between(xa1, xa2, xb2):
+            return xb2, yb2
+        else:
+            return "No Intercept"
+
+    i_x = (bb - ba) / (ma - mb)
+    i_y = ma * i_x + ba
+    if (between(xa1,xa2,i_x) and between(xb1,xb2,i_x) and
+        between(ya1,ya2,i_y) and between(yb1,yb2,i_y)):
+        return i_x, i_y
+    else:
+        return "No Intercept"
+
+
+def vert(x1,x2):
+    if x1 == x2:
+        return True
+
+
+def slope_int(x1,x2,y1,y2):
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+    return m, b
+
+
+def between(p1,p2,pi):
+    if p1 <= pi <= p2 or p2 <= pi <= p1:
+        return True
+    else:
+        return False
