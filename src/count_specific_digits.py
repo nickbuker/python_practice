091dcdbcc2be46bf13bfab67f5059110e137a277
nickbuker <nickbuker@gmail.com>
2017-01-16 15:30:18 -0800

    add python practice

---
+++ src/count_specific_digits.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+class List(object):
+    def count_spec_digits(self, ints, digs):
+        counts = Counter("".join(map(str, ints)))
+        return [(int(k),counts[k]) if k in counts else (int(k),0)
+                for k in map(str, digs)]
