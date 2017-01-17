b4a7656dbdfef690321ddefed28fd090f363fa40
nickbuker <nickbuker@gmail.com>
2017-01-17 14:13:09 -0800

    add python practice

---
+++ src/compare_powers.py
@@ -0,0 +1,6 @@
+def compare_powers(n1,n2):
+    if n1[0] == n2[0]:
+        return cmp(n2[1], n1[1])
+    if n1[1] == n2[1]:
+        return cmp(n2[0], n1[0])
+    return cmp(n2[0] ** n2[1], n1[0] ** n1[1])
