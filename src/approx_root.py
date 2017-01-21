70259aaf50faa76f19f08769145a8dca6c763306
nickbuker <nickbuker@gmail.com>
2017-01-21 09:23:35 -0800

    add python practice

---
+++ src/approx_root.py
@@ -0,0 +1,10 @@
+def approx_root(n):
+    base = 0
+    for i in xrange(1,n):
+        if i ** 2 <= n:
+            base = i
+        if i ** 2 > n:
+            break
+    diff_gn = n - (base ** 2)
+    diff_lg = ((base + 1) ** 2) - (base ** 2)
+    return round(base + (diff_gn / float(diff_lg)), 2)
