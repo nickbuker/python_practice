02abd8e19cda36c6a425cf2c4e2009b9075b2c5e
nickbuker <nickbuker@gmail.com>
2017-01-21 20:19:31 -0800

    add python practice

---
+++ src/word_pattern.py
@@ -0,0 +1,4 @@
+def word_pattern(pattern, string):
+    mapping =  dict(zip(str(pattern), string.split()))
+    return " ".join([mapping[c] for c in pattern]) == string \
+    and len(mapping) == len(set(mapping.values()))
