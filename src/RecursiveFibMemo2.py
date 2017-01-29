f6f6aec3c47ebd0ff74b3826234f0a4e0e81d024
nickbuker <nickbuker@gmail.com>
2017-01-28 17:55:11 -0800

    add python practice

---
+++ src/RecursiveFibMemo2.py
@@ -0,0 +1,13 @@
+class Fib(object):
+
+    def __init__(self):
+        self.memo = {}
+
+    def fib(self, n):
+        if n in self.memo:
+            return self.memo[n]
+        if n < 2:
+            return n
+        result = self.fib(n - 1) + self.fib(n - 2)
+        self.memo[n] = result
+        return result
