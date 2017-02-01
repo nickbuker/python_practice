72539a87a93dc925b21d05deceef5daba58da0af
nickbuker <nickbuker@gmail.com>
2017-02-01 11:56:06 -0800

    add python practice

---
+++ src/count_cows.py
@@ -0,0 +1,16 @@
+def count_cows(n):
+    if type(n) != int:
+        return None
+    cow_num = 1
+    cows = {0:0}
+    cows_temp = {}
+    for i in xrange(n):
+        for cow in cows:
+            cows[cow] += 1
+        for age in cows.values():
+            if age >= 3:
+                cows_temp[cow_num] = 0
+                cow_num += 1
+        cows.update(cows_temp)
+        cows_temp = {}
+    return len(cows)
---
+++ src/sequence_sum.py
@@ -0,0 +1,2 @@
+def sequence_sum(beg, end, step):
+    return sum(range(beg, end + 1, step))
