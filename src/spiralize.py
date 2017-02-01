b0ae93feb86754f1ec86c2aee36cdb07724b30b1
nickbuker <nickbuker@gmail.com>
2017-02-01 11:12:49 -0800

    add python practice

--- src/spiralize.py
+++ src/spiralize.py
@@ -18,5 +18,34 @@ def add_ones(array):
         return [[1]]
     if len(array) == 2:
         return [[1,1][0,1]]
-    pass
-    # finish me 
+    for n in array[0]:
+        n = 1
+    for n in array[-1]:
+        n = 1
+    i = 1
+    starts = []
+    stops = [-1]
+    while i < len(array) - 1:
+        if i < len(array) - 1:
+            for start in starts:
+                array[i][start] = 1
+            if len(starts) == 0:
+                starts.append(0)
+            else:
+                starts.append(starts[-1] + 2)
+            for stop in stops:
+                array[i][stop] = 1
+            stops.append(stops[-1] - 2)
+        i += 1
+        if i < len(array) - 1:
+            for start in starts:
+                array[-i][start] = 1
+            if len(starts) == 0:
+                starts.append(0)
+            else:
+                starts.append(starts[-1] + 2)
+            for stop in stops:
+                array[-i][stop] = 1
+            stops.append(stops[-1] - 2)
+        i += 1
+        
