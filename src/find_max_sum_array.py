48dec8b32d7f283c98201c341e99671d33474c30
nickbuker <nickbuker@gmail.com>
2017-02-08 13:53:11 -0800

    add python practice

--- src/find_max_sum_array.py
+++ src/find_max_sum_array.py
@@ -2,11 +2,22 @@ import numpy as np
 
 
 def find_max(arr):
-    rows, cols, diags = find_segs(arr)
+    '''
+    Input: list of lists containing integers
+    Output: max 3 element sum from list of lists
+    Calls make_segs() to find valid indices and calls get_max_sum() to
+    find max 3 element sum
+    '''
+    rows, cols, diags = make_segs(arr)
     return get_max_sum(rows, cols, diags, arr)
 
 
-def find_segs(arr):
+def make_segs(arr):
+    '''
+    Input: list of lists containing integers
+    Output: valid indices for row, col and diag sums
+    Validates indices for get_max_sum() to avoid IndexErrors
+    '''
     rows, cols, diags = [], [], []
     for i, row in enumerate(arr):
         for j in range(0,len(arr)):
@@ -20,6 +31,13 @@ def find_segs(arr):
 
 
 def get_max_sum(rows, cols, diags, arr):
+    '''
+    Input: lists of valid row, col, diag indices from make_segs()
+    and list of lists containing integers
+    Output: largest 3 element sum from list of lists
+    Checks every valid horizontal, vertical, and diagonal 3 element sum
+    and returns max_sum
+    '''
     max_sum = -np.inf
     for n in rows:
         row_sum = (
