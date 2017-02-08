c15c4fd084daee1741daeb6f641959288e522ab5
nickbuker <nickbuker@gmail.com>
2017-02-08 11:42:24 -0800

    add python practice

---
+++ src/find_max_sum_array.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+def find_max(arr):
+    rows, cols, diags = find_segs(arr)
+    return get_max_sum(rows, cols, diags, arr)
+
+
+def find_segs(arr):
+    rows, cols, diags = [], [], []
+    for i, row in enumerate(arr):
+        for j in range(0,len(arr)):
+            if j + 2 < len(row):
+                rows.append((i, j))
+            if i + 2 < len(arr):
+                cols.append((i, j))
+            if j + 2 < len(row) and i + 2 < len(arr):
+                diags.append((i, j))
+    return rows, cols, diags
+
+
+def get_max_sum(rows, cols, diags, arr):
+    max_sum = -np.inf
+    for n in rows:
+        row_sum = (arr[n[0]][n[1]] +
+                   arr[n[0]][n[1] + 1] +
+                   arr[n[0]][n[1] + 2])
+        if max_sum < row_sum:
+            max_sum = row_sum
+    for n in cols:
+        col_sum = (arr[n[0]][n[1]] +
+                   arr[n[0] + 1][n[1]] +
+                   arr[n[0] + 2][n[1]])
+        if max_sum < col_sum:
+            max_sum = col_sum
+    for n in diags:
+        diag_sum = (arr[n[0]][n[1]] +
+                   arr[n[0] + 1][n[1] + 1] +
+                   arr[n[0] + 2][n[1] + 2])
+        if max_sum < diag_sum:
+            max_sum = diag_sum
+    return max_sum
