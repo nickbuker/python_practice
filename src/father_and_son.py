ff52653937f1ff7610b8d34476074f8ab0dc81dd
nickbuker <nickbuker@gmail.com>
2017-02-13 14:53:01 -0800

    add python practice

---
+++ src/father_and_son.py
@@ -0,0 +1,5 @@
+def sc(s):
+    uppers = [c for c in s if c == c.upper()]
+    lowers = [c for c in s if c == c.lower()]
+    return "".join([c for c in s if c.upper() in uppers
+                                and c.lower() in lowers])
