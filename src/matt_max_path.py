9b2f3104c1e124798021b9b9c85524b2e055f717
nickbuker <nickbuker@gmail.com>
2017-02-23 09:55:11 -0800

    update max path

--- src/matt_max_path.py
+++ src/matt_max_path.py
@@ -4,6 +4,13 @@ import numpy as np
 def find_max_path(arr):
     shape = (len(arr), len(arr[0]))
     paths = make_paths_matrix(shape)
+    for col in xrange(1, shape[1]):
+        windows = make_windows(col, arr, shape)
+        paths = find_best_windows(col, arr, shape, windows, paths)
+        if col == shape[1] - 1:
+            col_list_arr = [row[col] for row in arr]
+            col_list_paths = [row[col] for row in paths]
+            return max([sum(x) for x in zip(col_list_arr, col_list_paths)])
 
 
 def make_paths_matrix(shape):
@@ -16,14 +23,14 @@ def make_paths_matrix(shape):
 
 def make_windows(col, arr, shape):
     windows = {}
-    col_list = [row[col] for row in arr]
+    col_list = [row[col - 1] for row in arr]
     for i in xrange(shape[1]):
         for j in xrange(i, shape[1]):
-            windows[((i, col), (j, col))] = sum(col_list[i: j + 1])
+            windows[((i, col - 1), (j, col - 1))] = sum(col_list[i: j + 1])
     return windows
 
 
-def find_best_windows(col, shape, arr, windows, paths):
+def find_best_windows(col, arr, shape, windows, paths):
     best_windows = {}
     for i in xrange(shape[0]):
         best_windows[(i, col)] = (0, -np.inf)
@@ -34,4 +41,4 @@ def find_best_windows(col, shape, arr, windows, paths):
                     paths[i][col] = v + max(paths[k[0][0]][k[0][1]],
                                             paths[k[1][0]][k[1][1]])
                     best_windows[(i, col)] = (k, v)
-    return best_windows, paths
+    return paths
