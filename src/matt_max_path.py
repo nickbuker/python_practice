fix calculation problem

src/matt_max_path.py
@@ -6,8 +6,8 @@ def find_max_path(arr):
     paths = make_paths_matrix(shape)
     for col in xrange(1, shape[1]):
         windows = make_windows(col, arr, shape)
-        paths = find_best_windows(col, arr, shape, windows, paths)
-        print paths
+        filtered_windows = filter_windows(col, windows, shape)
+        paths = find_best_windows(col, filtered_windows, paths, shape)
         if col == shape[1] - 1:
             col_list_arr = [row[col] for row in arr]
             col_list_paths = [row[col] for row in paths]
@@ -31,15 +31,29 @@ def make_windows(col, arr, shape):
     return windows
 
 
-def find_best_windows(col, arr, shape, windows, paths):
+def filter_windows(col, windows, shape):
+    filtered_windows = {}
+    for i in xrange(shape[0]):
+        if (i, col) not in filtered_windows:
+            filtered_windows[(i, col)] = []
+        for k, v in windows.iteritems():
+            if (i, col - 1) == k[0] or (i, col - 1) == k[1]:
+                filtered_windows[(i, col)].append((k, v))
+    return filtered_windows
+
+
+def find_best_windows(col, filtered_windows, paths, shape):
     best_windows = {}
     for i in xrange(shape[0]):
-        best_windows[(i, col)] = (0, -np.inf)
-        for k, v in windows.iteritems():
-            if k[0] == (i, col - 1) or k[1] == (i, col - 1):
-                if best_windows[(i,col)][1] < v + max(paths[k[0][0]][k[0][1]],
-                                                      paths[k[1][0]][k[1][1]]):
-                    paths[i][col] = v + max(paths[k[0][0]][k[0][1]],
-                                            paths[k[1][0]][k[1][1]])
-                    best_windows[(i, col)] = (k, v)
+        print i, col
+        if (i, col) not in best_windows:
+            best_windows[(i, col)] = (((-1,-1), (-1,-1)), -np.inf)
+        for n in filtered_windows[(i, col)]:
+            k0, k1, win_sum = n[0][0], n[0][1], n[1]
+            path = max(paths[k0[0]][k0[1]], paths[k1[0]][k1[1]])
+            total = win_sum + path
+            if best_windows[(i, col)][1] < total:
+                paths[i][col] = total
+                best_windows[(i, col)] = ((k0, k1), total)
+            print k0, k1, win_sum, total
     return paths