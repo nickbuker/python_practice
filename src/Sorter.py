8393e54a59e31a994216075c1e68ed058e2462e2
nickbuker <nickbuker@gmail.com>
2017-03-01 13:21:52 -0800

    add merge sort

--- src/Sorter.py
+++ src/Sorter.py
@@ -5,14 +5,18 @@ class Sorter(object):
 
     def __init__(self, arr):
         self.arr = arr
-        self.comp, self.edits = 0, 0
 
     def sort(self, sort_type='bubble'):
         time0 = time.time()
         if sort_type == 'bubble':
+            self.comp, self.edits = 0, 0
             sorted_arr = self._bubble(self.arr)
         elif sort_type == 'quick':
+            self.comp, self.edits = 0, 0
             sorted_arr = self._quicksort(self.arr)
+        elif sort_type == 'merge':
+            self.comp, self.edits = 0, 0
+            sorted_arr = self._merge_sort(self.arr)
         else:
             print 'Not valid sort technique.'
         print '\ncomparisons: {} \nedits: {}'.format(self.comp, self.edits)
@@ -22,7 +26,6 @@ class Sorter(object):
     def _bubble(self, arr):
         new_arr = copy(arr)
         active = True
-        self.comp, self.edits = 0, 0
         while active:
             active = False
             for i in xrange(1, len(new_arr)):
@@ -58,4 +61,31 @@ class Sorter(object):
             return new_arr
 
     def _merge_sort(self, arr):
-        pass
+        if len(arr) > 1:
+            split = len(arr) // 2
+            left, right = arr[:split], arr[split:]
+            self.edits += 2
+            sorted_left = self._merge_sort(left)
+            sorted_right = self._merge_sort(right)
+            new_arr = []
+            i, j = 0, 0
+            while i < len(sorted_left) and j < len(sorted_right):
+                self.comp += 1
+                if sorted_left[i] < sorted_right[j]:
+                    new_arr.append(sorted_left[i])
+                    i += 1
+                    self.edits += 1
+                else:
+                    new_arr.append(sorted_right[j])
+                    j += 1
+                    self.edits +=1
+            self.comp += 1
+            if i < len(sorted_left):
+                new_arr += sorted_left[i:]
+                self.edits += 1
+            self.comp += 1
+            if j < len(sorted_right):
+                new_arr += sorted_right[j:]
+                self.edits += 1
+            return new_arr
+        return arr
--- src/merge_sort.py
+++ src/merge_sort.py
@@ -1,29 +1,21 @@
-from collections import deque
-
 def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    if len(arr) == 2:
-        if arr[0] < arr[1]:
-            return arr
-        else:
-            return arr[::-1]
-    split = (len(arr) / 2) + 1
-    left = deque(merge_sort(arr[:split]))
-    right = deque(merge_sort(arr[split:]))
-    if left == arr[:split] and right == arr[split:]:
-        pass
-
-def merge(left, right):
-    sorted_arr = []
-    while len(left) > 0 or len(right) > 0:
-        if len(right) == 0 and len(left) > 0:
-            sorted_arr.append(left.popleft())
-        if len(left) == 0 and len(right) > 0:
-            sorted_arr.append(right.popleft())
-        if len(left) > 0 and len(right) > 0:
-            if left[0] <= right[0]:
-                sorted_arr.append(left.popleft())
-            elif left[0] > right[0]:
-                sorted_arr.append(right.popleft())
-    return sorted_arr
+    if len(arr) > 1:
+        split = len(arr) // 2
+        left, right = arr[:split], arr[split:]
+        sorted_left = merge_sort(left)
+        sorted_right = merge_sort(right)
+        sorted_arr = []
+        i, j = 0, 0
+        while i < len(sorted_left) and j < len(sorted_right):
+            if sorted_left[i] < sorted_right[j]:
+                sorted_arr.append(sorted_left[i])
+                i += 1
+            else:
+                sorted_arr.append(sorted_right[j])
+                j += 1
+        if i < len(sorted_left):
+            sorted_arr += sorted_left[i:]
+        if j < len(sorted_right):
+            sorted_arr += sorted_right[j:]
+        return sorted_arr
+    return arr
