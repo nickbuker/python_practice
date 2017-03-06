9228502a5281411115aa43261ff49208c8d73957
nickbuker <nickbuker@gmail.com>
2017-03-06 13:08:25 -0800

    Add DNA Sequence Tester

---
+++ src/check_dna.py
@@ -0,0 +1,5 @@
+def check_DNA(seq1, seq2):
+    seq2 = seq2[::-1]
+    pairs = {'G':'C', 'C':'G', 'A':'T', 'T':'A'}
+    seq1_conj = ''.join([pairs[base] for base in seq1])
+    return (seq1_conj in seq2) or (seq2 in seq1_conj)
