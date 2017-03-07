f6de9087f8124deeb52c510e4698b22eac2123b1
nickbuker <nickbuker@gmail.com>
2017-03-07 15:32:32 -0800

    add when I miss a few gym days

---
+++ src/miss_few_days_at_gym.py
@@ -0,0 +1,10 @@
+def gym_slang(s):
+    s = s.replace("robably", "rolly")
+    s = s.replace("I am", "I'm")
+    s = s.replace("i am", "i'm")
+    s = s.replace("nstagram", "nsta")
+    s = s.replace("o not", "on't")
+    s = s.replace("oing to", "onna")
+    s = s.replace("ombination", "ombo")
+    return s
+    
