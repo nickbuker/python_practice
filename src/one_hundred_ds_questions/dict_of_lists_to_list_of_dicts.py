3830c0581a0b2a85d34bc4c2b245be388461447a
nickbuker <nickbuker@gmail.com>
2017-04-03 11:30:12 -0700

    add question

--- src/one_hundred_ds_questions/dict_of_lists_to_list_of_dicts.py
+++ src/one_hundred_ds_questions/dict_of_lists_to_list_of_dicts.py
@@ -1,6 +1,7 @@
 """
 Write a function that converts a dictionary of equal length
 lists into a list of dictionaries.
+
 {'a': [1, 2, 3], 'b': [3, 2, 1]}
   => [{'a': 1, 'b': 3}, {'a': 2, 'b': 2}, {'a': 3, 'b': 1}]
 """
